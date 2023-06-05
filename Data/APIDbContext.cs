10bf662a0cb8301ad4f1f8b183c9d35d0827e35b
Fernando <cara-se@hotmail.com>
2023-06-05 20:50:21 -0300

    first commit

---
+++ Data/APIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QueroServicos.Models;
+
+namespace QueroServicos.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options): base(options)
+        {
+
+        }
+       
+      public DbSet<Categorias> Categorias { get; set; }
+    }
+}
---
+++ Models/Categorias.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QueroServicos.Models
+{
+    public class Categorias
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Campo Nome é Obrigatorio")]
+        public string Nome { get; set; }
+
+    }
+}
