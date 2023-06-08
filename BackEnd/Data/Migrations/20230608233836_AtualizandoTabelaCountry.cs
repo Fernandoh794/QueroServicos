8905c175c12270ba5fe1313de0cfaa6adaf5fd88
AlessonCC <alesson.carlos@academico.ifs.edu.br>
2023-06-08 20:57:23 -0300

    Atualizando Tabela Country

---
+++ BackEnd/Data/Migrations/20230608233836_AtualizandoTabelaCountry.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QueroServicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizandoTabelaCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Acronym",
+                table: "Countries",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Bacen",
+                table: "Countries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "name_pt",
+                table: "Countries",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Acronym",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "Bacen",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "name_pt",
+                table: "Countries");
+        }
+    }
+}
--- BackEnd/Models/Country.cs
+++ BackEnd/Models/Country.cs
@@ -9,5 +9,12 @@ namespace QueroServicos.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        [Column("name_pt")]
+        public string NamePt { get; set; }
+
+        public string Acronym { get; set; }
+
+        public int? Bacen { get; set; }
     }
 }
