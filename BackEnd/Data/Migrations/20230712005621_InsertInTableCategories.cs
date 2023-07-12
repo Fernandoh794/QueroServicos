5fb25d51b9b1f80b6e093298de8a5072dc0ae967
alessoncc <alessoncarlos20@gmail.com>
2023-07-11 22:15:57 -0300

    Populando tabelas Category e Subcategories

---
+++ BackEnd/Data/Migrations/20230712005621_InsertInTableCategories.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QueroServicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertInTableCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO categories (Description) VALUES ('Peças e Auto'), ('Assistência Técnica'), ('Aulas'), ('Design e Software'), ('Eventos'), ('Moda e Beleza'), ('Reparos e Reformas'), ('Serviços Domésticos');\r\n");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
